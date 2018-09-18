ca8ef2b531b2fa38855a2e711ada8d0ea5bb08b6
Tyler <tbh717@gmail.com>
2018-09-18 17:47:48 -0500

    push for pc

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    public GameObject gameBoard;
+    private ArrayList tiles;
+
+	// Use this for initialization
+	void Start () {
+		gameBoard.childre
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TileInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInteraction : MonoBehaviour {
+
+    private Color primaryColor;
+
+	// Use this for initialization
+	void Start () {
+		primaryColor = GetComponent<SpriteRenderer>().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver () {
+        ChangeColor(Color.red);
+    }
+
+    void OnMouseExit () {
+        ChangeColor(primaryColor);
+    }
+
+    void ChangeColor (Color newColor) {
+        transform.GetComponent<SpriteRenderer>().color = newColor;
+    }
+}
