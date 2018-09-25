b10f581c392adec72d38b986e9047f1e258d55c0
Tyler Herron <tbh717@gmail.com>
2018-09-25 10:42:49 -0500

    Changes for laptop

---
+++ cosmilcuture-game/Assets/Scripts/Items/Flora.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class Magnolium : Flora {
+//     private GameObject tile;
+//     private Sprite sprite;
+
+//     public int baseScore = 2;
+
+//     public override int ItemScore() {
+//         return 1;
+//     }
+// }
